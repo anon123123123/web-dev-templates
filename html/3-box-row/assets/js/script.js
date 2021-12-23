f0e4487d7e57b7abde4c559e84839d3ff0cce366
anon123123123 <cditodavis96@gmail.com>
2021-12-22 21:28:23 -0700

    Made HTML 3 box template

---
+++ html/3-box-row/assets/js/script.js
@@ -0,0 +1,23 @@
+// Checks to see if darkmode is in LS for persistence 
+function checkLocalStorageDM() {
+    if(window.localStorage.getItem('darkmode')){
+      darkModeTest()
+    } else {
+      return
+    }
+  }
+  //Switches to darkmode
+  function darkModeTest() {
+    // Toggles the CSS class
+    document.body.classList.toggle('dark-mode')
+    if (document.body.classList.contains('dark-mode')) {
+      // If darkmode put object in LS
+      window.localStorage.setItem('darkmode', true)
+      console.log('[!] Dark Mode Enabled')
+    } else {
+      console.log('[!] Light Mode Enabled')
+      window.localStorage.removeItem('darkmode')
+    }
+  }
+  // Runs the check on page load 
+  checkLocalStorageDM()

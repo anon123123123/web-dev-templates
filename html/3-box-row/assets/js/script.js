6aa7fc64ea440c4fcec5cb0998e497a5f377ddab
anon123123123 <cditodavis96@gmail.com>
2021-12-22 23:16:51 -0700

    Placed all objects excluding images - added color schema to html 3-box template

--- html/3-box-row/assets/js/script.js
+++ html/3-box-row/assets/js/script.js
@@ -1,23 +1,25 @@
 // Checks to see if darkmode is in LS for persistence 
 function checkLocalStorageDM() {
-    if(window.localStorage.getItem('darkmode')){
-      darkModeTest()
+    if (window.localStorage.getItem('darkmode')) {
+        darkModeTest()
     } else {
-      return
+        return
     }
-  }
-  //Switches to darkmode
-  function darkModeTest() {
+}
+//Switches to darkmode
+function darkModeTest() {
     // Toggles the CSS class
     document.body.classList.toggle('dark-mode')
     if (document.body.classList.contains('dark-mode')) {
-      // If darkmode put object in LS
-      window.localStorage.setItem('darkmode', true)
-      console.log('[!] Dark Mode Enabled')
+        // If darkmode put object in LS
+        window.localStorage.setItem('darkmode', true)
+        console.log('[!] Dark Mode Enabled')
     } else {
-      console.log('[!] Light Mode Enabled')
-      window.localStorage.removeItem('darkmode')
+        console.log('[!] Light Mode Enabled')
+        window.localStorage.removeItem('darkmode')
     }
-  }
-  // Runs the check on page load 
-  checkLocalStorageDM()
+}
+// Runs the check on page load 
+checkLocalStorageDM()
+
+document.getElementById("darkmode-button").addEventListener("click", darkModeTest);
